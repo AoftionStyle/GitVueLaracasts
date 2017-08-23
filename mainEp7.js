d07763758943ea2eda3a21fb87b592ffa603f351
Chutipon Pongpanit <chutiponpon@7eleven.cp.co.th>
2017-08-23 20:15:31 +0700

    component <task-list> call component task

--- mainEp7.js
+++ mainEp7.js
@@ -1,6 +1,24 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks" :key=task.tasker>{{ task.tasker }}</task>
+        </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                { tasker: "one", complete:true },
+                { tasker: "two", complete:false },
+                { tasker: "three", complete:true },
+                { tasker: "four", complete:false},
+            ]
+        }
+    }
+})
+
 Vue.component('task', {
-    
-    template: '<li><slot><slot></li>'
+
+    template: '<li><slot></slot></li>'
 })
 
 new Vue({
