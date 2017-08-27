8fd47658af5dade4a62c24771828ea319bb43553
Chutipon.ppn <chutipon.ppn@gmail.com>
2017-08-27 23:47:27 +0700

    tabs available

---
+++ mainEp11.js
@@ -0,0 +1,19 @@
+Vue.component('tabs', {
+  template: `
+    <div class="tabs">
+      <ul>
+        <li class="is-active"><a>Pictures</a></li>
+        <li><a>Music</a></li>
+        <li><a>Videos</a></li>
+        <li><a>Documents</a></li>
+      </ul>
+    </div>
+  `,
+  mounted(){
+    console.log(this.$children)
+  }
+})
+
+new Vue({
+  el: '#root'
+})
