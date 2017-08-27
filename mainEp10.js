41dfae778a01ad37bcf28afdcadc26c0a7ff364f
Chutipon.ppn <chutipon.ppn@gmail.com>
2017-08-27 22:09:25 +0700

    move modal to template vue componet

---
+++ mainEp10.js
@@ -0,0 +1,17 @@
+Vue.component('modal', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <p style="color:coral">I am aoftion, I am Smart, I will get it</p>
+          <p style="color:red">This is a paragraph.</p>
+        </div>
+      </div>
+    </div>
+  `
+})
+
+new Vue({
+  el: '#root'
+})
